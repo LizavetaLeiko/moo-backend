37f253303655ef37d7374d1983bd8d30a2297f41
LizavetaLazarchyk <lizaleiko3@gmail.com>
2023-01-02 12:21:01 +0300

    fix cookie

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -17,6 +17,7 @@ class UserController {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: true,
+        sameSite: 'none'
       });
       return res.json(userData);
     } catch (e) {
@@ -31,6 +32,8 @@ class UserController {
       res.cookie("refreshToken", userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        secure: true,
+        sameSite: 'none'
       });
       return res.json(userData);
     } catch (e) {
